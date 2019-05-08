dbd358a508f1b3623de5f3bc5304cae447808441
Philip Krause <philipkrause7@gmail.com>
2019-05-08 17:21:40 -0400

    v1

--- public/main.js
+++ public/main.js
@@ -1,7 +1,35 @@
+let cardNum = ['A', 2, 3, 3, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+let cardSuit = ['hearts', 'clubs', 'diamonds', 'spades']
+
+let deck = []
+
+let chooseCard = () => {
+  let randomCard = [Math.floor(Math.random() * deck.length)]
+  const cardHolder = document.createElement('section')
+  cardHolder.textContent = deck[randomCard]
+  document.querySelector('.container').appendChild(cardHolder)
+  console.log(chooseCard)
+}
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  makeDeck()
+  chooseCard()
 }
 
+const makeDeck = () => {
+  for (let x = 0; x <= cardNum.length - 1; x++) {
+    for (let y = 0; y <= cardSuit.length - 1; y++) {
+      deck.push(cardNum[x] + ' ' + 'of' + ' ' + cardSuit[y])
+      console.log(deck)
+    }
+  }
+}
+//    color.forEach(color =>{
+//     position.forEach(position => {
+//       chessSet.push(color + position)
+//     })
+// })
+
+document
+  .querySelector('.container.deal-button')
+  .addEventListener('click', chooseCard)
 document.addEventListener('DOMContentLoaded', main)
